bffdd22423ae32f90c9fcca8d7dd5012e8f85a26
Kamil Malinowski <kamil.malinowski.mlu@gmail.com>
2020-12-15 13:10:14 +0100

    feat: update styles and js

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const jump = document.querySelector(".jump--js");
 const sprite = document.querySelector(".sprite--js");
 const js = document.querySelector(".js--js");
 
+const score = document.querySelector(".score--js");
+
 //buttons
 //1. play
 
@@ -24,21 +26,25 @@ play.addEventListener("click", () => {
   const area = document.querySelector(".area--js");
   if (isOff) {
     area.style.display = "none";
-
     isOff = false;
   } else {
-    area.style.display = "block";
-    isOff = true;
+    setTimeout(() => {
+      area.style.display = "block";
+      isOff = true;
+    }, 800);
   }
 });
 
 //2. jump
-jump.addEventListener("click", () => {
-  if (sprite) {
+jump.addEventListener("click", (e) => {
+  if (e) {
     sprite.classList.add("animation");
+    score.innerHTML++;
   }
+
   setTimeout(() => {
     sprite.classList.remove("animation");
+
   }, 400);
 });
 
