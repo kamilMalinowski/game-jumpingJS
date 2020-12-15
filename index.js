0fa85c70e40359bfb8557d12c599d72535fd7150
Kamil Malinowski <kamil.malinowski.mlu@gmail.com>
2020-12-15 10:26:37 +0100

    first commit - import game from awesome-notes repository

---
+++ index.js
@@ -0,0 +1,54 @@
+//GAME-JUMP ('notatki-js.html') - WORK IN PROGRESS
+// 1.
+// 2.
+// 3.
+// 4.
+// 5.
+// 6.
+// 7.
+// 8.
+// 9.
+// 10.
+
+const play = document.querySelector(".play--js");
+const jump = document.querySelector(".jump--js");
+
+const sprite = document.querySelector(".sprite--js");
+const js = document.querySelector(".js--js");
+
+//buttons
+//1. play
+
+let isOff = false;
+play.addEventListener("click", () => {
+  const area = document.querySelector(".area--js");
+  if (isOff) {
+    area.style.display = "none";
+
+    isOff = false;
+  } else {
+    area.style.display = "block";
+    isOff = true;
+  }
+});
+
+//2. jump
+jump.addEventListener("click", () => {
+  if (sprite) {
+    sprite.classList.add("animation");
+  }
+  setTimeout(() => {
+    sprite.classList.remove("animation");
+  }, 400);
+});
+
+const gameOver = setInterval(() => {
+  let spriteTop = parseInt(
+    window.getComputedStyle(sprite).getPropertyValue("top")
+  );
+  let jsLeft = parseInt(window.getComputedStyle(js).getPropertyValue("left"));
+  if (jsLeft < 25 && jsLeft > -25 && spriteTop >= 135) {
+    js.style.animation = "none";
+    js.style.left = "50px";
+  }
+}, 10);
